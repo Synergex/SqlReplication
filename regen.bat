5f8a9b41a71269c72fb56747276f83f633d888da
Steve Ives <steve.ives@synergex.com>
2016-07-24 01:27:38 -0400

    Switched out ONERROR code for TRY CATCH code

--- regen.bat
+++ regen.bat
@@ -3,15 +3,15 @@ setlocal
 
 rem Configure the CodeGen environment
 
-set ROOT_FOLDER=%~dp0
-set CODEGEN_TPLDIR=%ROOT_FOLDER%\SRC\TEMPLATES
+set ROOT=%~dp0
+set CODEGEN_TPLDIR=%ROOT%\SRC\TEMPLATES
 set CODEGEN_AUTHOR="Steve Ives"
 set CODEGEN_COMPANY="Synergex Professional Services Group"
 set OPTS=-e -r -lf -n SynPSG.ReplicationDemo
 
-codegen %OPTS% -s EMPLOYEE   -t SynIO SqlIO ReplicationIoHooks IsNumeric -o "%ROOT_FOLDER%\SRC\LIBRARY" -define ATTACH_IO_HOOKS
+codegen %OPTS% -s EMPLOYEE   -t SynIO SqlIO ReplicationIoHooks IsNumeric -o "%ROOT%\SRC\LIBRARY" -define ATTACH_IO_HOOKS
 
-codegen %OPTS% -s DEPARTMENT -t SynIO                                    -o "%ROOT_FOLDER%\SRC\LIBRARY"
+codegen %OPTS% -s DEPARTMENT -t SynIO                                    -o "%ROOT%\SRC\LIBRARY"
 
 
 endlocal
