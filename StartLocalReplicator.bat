27d78ecf2b80138635224e76dbdc020b82b658eb
Steve Ives <steve.ives@synergex.com>
2017-08-04 13:45:54 -0700

    Code cleanup in replicator. Removed all the COMMON's.

--- StartLocalReplicator.bat
+++ StartLocalReplicator.bat
@@ -1,2 +1,5 @@
 @echo off
 dbr EXE:replicator -interval 2 -verbose -keyvalues -loaderrors -erroremail steve.ives@synergex.com -mailserver exch2016.synergex.loc -mailfrom replicator@synergex.com -maildomain synergex.com -stoponerror -database VTX12_SQLNATIVE://SqlReplicationIoHooks/.///Trusted_connection=yes
+
+rem This should work, but currently doesn't. Roger is working on it with Trifox
+rem dbr -d EXE:replicator -interval 2 -verbose -keyvalues -loaderrors -erroremail steve.ives@synergex.com -mailserver exch2016.synergex.loc -mailfrom replicator@synergex.com -maildomain synergex.com -stoponerror -database net://SqlReplicationIoHooks/.///Trusted_connection=yes@1958:LOCALHOST!VTX12_SQLNATIVE
