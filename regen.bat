47c629f39330b1248668b3b3b23628a2c39775ef
Steve Ives <steve@steveives.com>
2019-02-01 12:00:24 -0500

    Final changes before moving to HGV DevOps server

--- regen.bat
+++ regen.bat
@@ -4,12 +4,12 @@ setlocal
 rem ---------------------------------------------------------------------------
 rem set code generation options
 
-set STRUCTURES=EMPLOYEE
+set STRUCTURES=TYPMAS UNTMAS
 
-rem set USE_STRUCTURE_ALIASES=-a %STRUCTURES%
+set USE_STRUCTURE_ALIASES=-a UNIT_TYPE UNIT
 
 set ENABLE_CLEAN_DATA=-define CLEAN_DATA
-rem set USE_ALTERNATE_FIELD_NAMES=-af
+set USE_ALTERNATE_FIELD_NAMES=-af
 
 rem ---------------------------------------------------------------------------
 rem Configure the CodeGen environment
@@ -18,13 +18,13 @@ set ROOT=%~dp0
 set CODEGEN_TPLDIR=%ROOT%\SRC\TEMPLATES
 set CODEGEN_OUTDIR=%ROOT%\SRC\LIBRARY
 
-set STDOPTS=-e -r -lf %USE_STRUCTURE_ALIASES% %USE_ALTERNATE_FIELD_NAMES% %ENABLE_CLEAN_DATA%
+set STDOPTS=-e -r -lf -u UserDefinedTokens.tkn %USE_STRUCTURE_ALIASES% %USE_ALTERNATE_FIELD_NAMES% %ENABLE_CLEAN_DATA%
 
 rem ---------------------------------------------------------------------------
 rem Generate code
 
 rem Generate SQL I/O routines for the structures being replicated
-codegen  -s %STRUCTURES% -t SqlIO %STDOPTS%
+codegen  -s %STRUCTURES% -t SqlIO CosmosIO %STDOPTS%
 
 rem Templates requiring all structures to be processed at once
 codegen -s %STRUCTURES% -ms -t GetReplicatedTables %STDOPTS%
