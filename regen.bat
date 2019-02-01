255e8e51e72ca77dacfc4daad71e47a8e32a51c6
Steve Ives <steve@steveives.com>
2019-02-01 12:18:41 -0500

    Revert to two commits ago!

--- regen.bat
+++ regen.bat
@@ -4,12 +4,12 @@ setlocal
 rem ---------------------------------------------------------------------------
 rem set code generation options
 
-set STRUCTURES=TYPMAS UNTMAS
+set STRUCTURES=EMPLOYEE
 
-set USE_STRUCTURE_ALIASES=-a UNIT_TYPE UNIT
+rem set USE_STRUCTURE_ALIASES=-a %STRUCTURES%
 
 set ENABLE_CLEAN_DATA=-define CLEAN_DATA
-set USE_ALTERNATE_FIELD_NAMES=-af
+rem set USE_ALTERNATE_FIELD_NAMES=-af
 
 rem ---------------------------------------------------------------------------
 rem Configure the CodeGen environment
@@ -18,13 +18,13 @@ set ROOT=%~dp0
 set CODEGEN_TPLDIR=%ROOT%\SRC\TEMPLATES
 set CODEGEN_OUTDIR=%ROOT%\SRC\LIBRARY
 
-set STDOPTS=-e -r -lf -u UserDefinedTokens.tkn %USE_STRUCTURE_ALIASES% %USE_ALTERNATE_FIELD_NAMES% %ENABLE_CLEAN_DATA%
+set STDOPTS=-e -r -lf %USE_STRUCTURE_ALIASES% %USE_ALTERNATE_FIELD_NAMES% %ENABLE_CLEAN_DATA%
 
 rem ---------------------------------------------------------------------------
 rem Generate code
 
 rem Generate SQL I/O routines for the structures being replicated
-codegen  -s %STRUCTURES% -t SqlIO CosmosIO %STDOPTS%
+codegen  -s %STRUCTURES% -t SqlIO %STDOPTS%
 
 rem Templates requiring all structures to be processed at once
 codegen -s %STRUCTURES% -ms -t GetReplicatedTables %STDOPTS%
