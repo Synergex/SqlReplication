806d5c1ccbe189494dd413d2fb90b5deacab5b51
Steve Ives <steve.ives@synergex.com>
2025-09-20 12:25:54 -0700

    Minor tweaks.

--- DAT/reset.bat
+++ DAT/reset.bat
@@ -1,4 +1,9 @@
 @echo off
+call "%SYNERGYDE64%dbl\dblvars64.bat"
 pushd %~dp0
+echo Extracting EMPLOYEE.ISM
 "C:\Program Files\7-zip\7z.exe" e -y EMPLOYEE_100K.7z
+echo OPTIMIZING REPLICATION_DEFAULT.ISM
+isutl -ro REPLICATION_DEFAULT.ISM
+pause
 popd
